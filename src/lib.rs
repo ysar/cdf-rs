2d3ab5ff03a3389608be86a1955f2f07ba1d3f6c
ysar <31999239+ysar@users.noreply.github.com>
2026-01-23 22:51:42 -0600

    no point in keep unsupported encodings as enum variants

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,4 @@ pub mod record;
 
 /// The structure of the CDF file.
 pub mod cdf;
+
--- src/repr.rs
+++ src/repr.rs
@@ -32,10 +32,6 @@ pub enum CdfEncoding {
     Next = 12,
     /// DEC Alpha/OSF1 Representation
     AlphaOsf1 = 13,
-    /// DEC Alpha/Open VMS Representation (Double precision floats in D_FLOAT encoding)
-    AlphaVmsD = 14,
-    /// DEC Alpha/Open VMS Representation (Double precision floats in G_FLOAT encoding)
-    AlphaVmsG = 15,
     /// DEC Alpha/Open VMS Representation (Single/Double precision floats in IEEE 754 encoding)
     AlphaVmsI = 16,
     /// ARM little-endian Representation
@@ -44,12 +40,6 @@ pub enum CdfEncoding {
     ArmBig = 18,
     /// Itanium 64 on OpenVMS Representation (Single/Double precision floats in IEEE 754 encoding)
     Ia64VmsI = 19,
-    /// Itanium 64 on OpenVMS Representation (Single/Double precision floats in Digital D_FLOAT
-    /// encoding)
-    Ia64VmsD = 20,
-    /// Itanium 64 on OpenVMS Representation (Single/Double precision floats in Digital G_FLOAT
-    /// encoding)
-    Ia64VmsG = 21,
 }
 
 impl CdfEncoding {
@@ -101,14 +91,10 @@ impl TryFrom<CdfInt4> for CdfEncoding {
             11 => Ok(CdfEncoding::Hp),
             12 => Ok(CdfEncoding::Next),
             13 => Ok(CdfEncoding::AlphaOsf1),
-            14 => Ok(CdfEncoding::AlphaVmsD),
-            15 => Ok(CdfEncoding::AlphaVmsG),
             16 => Ok(CdfEncoding::AlphaVmsI),
             17 => Ok(CdfEncoding::ArmLittle),
             18 => Ok(CdfEncoding::ArmBig),
             19 => Ok(CdfEncoding::Ia64VmsI),
-            20 => Ok(CdfEncoding::Ia64VmsD),
-            21 => Ok(CdfEncoding::Ia64VmsG),
             v => Err(CdfError::Decode(format!("Invalid encoding integer - {v}."))),
         }
     }
